38a5ee854f777ccaed24a44ea276b1bc3e002ae4
touchselect <kir191474@gmail.com>
2023-11-15 16:41:00 +0900

    add mapper test

--- src/test/java/com/example/todoapi2/mapper/TodoMapperTest.java
+++ src/test/java/com/example/todoapi2/mapper/TodoMapperTest.java
@@ -2,6 +2,8 @@ package com.example.todoapi2.mapper;
 
 import com.example.todoapi2.model.Status;
 import com.example.todoapi2.model.Todo;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,67 +20,138 @@ class TodoMapperTest {
     @Autowired
     private TodoMapper todoMapper;
 
+    // テスト用のTodoオブジェクトをセットアップするメソッド
+    private static Todo setUpTodo() {
+        Todo todo = new Todo();
+        todo.setId(999L); //できれば自動生成でやりたい
+        todo.setTitle("Test Todo");
+        todo.setStatus(Status.valueOf("未着手"));
+        todo.setDetails("hogehoge");
+        return todo;
+    }
+
+    // 全てのTodoを取得するテスト
     @Test
     void getAllTodos() {
+        // マッパーから全てのTodoを取得
         List<Todo> todos = todoMapper.getAllTodos();
+        // 取得されたリストがnullでないことを確認
         assertNotNull(todos);
     }
 
+    // ステータスに基づいてTodoを取得するテスト
     @Test
     void getTodoByStatus() {
+        // 各ステータスに基づいてTodoを取得
         List<Todo> todos1 = todoMapper.getTodoByStatus(Status.未着手);
         List<Todo> todos2 = todoMapper.getTodoByStatus(Status.進行中);
         List<Todo> todos3 = todoMapper.getTodoByStatus(Status.完了);
 
+        // 取得されたリストがnullでないことを確認
         assertNotNull(todos1);
         assertNotNull(todos2);
         assertNotNull(todos3);
     }
 
+    // タイトルに基づいてTodoを取得するテスト
     @Test
     void getTodoByTitle() {
+        // "ap"を含むタイトルのTodoを取得
         List<Todo> matchAP = todoMapper.getTodoByTitle("ap");
+        // "hoge"を含むタイトルのTodoを取得
         List<Todo> matchHoge = todoMapper.getTodoByTitle("hoge");
 
-        assertThat(matchAP, hasSize(1));
+        // "ap"を含むTodoが2つあることを確認
+        assertThat(matchAP, hasSize(2));
+        // "hoge"を含むTodoの最初のタイトルが"hoge"であることを確認
         assertThat(matchHoge.get(0).getTitle(), equalTo("hoge"));
     }
 
+    // IDに基づいてTodoを取得するテスト
     @Test
     void getTodoById() {
+        // IDが1のTodoを取得
         Todo todo = todoMapper.getTodoById(1L);
+        // 取得したTodoのタイトルが"タスク１"であることを確認
         assertThat(todo.getTitle(), equalTo("タスク１"));
     }
 
+    // Todoを挿入するテスト
     @Test
     void insertTodo() {
-        Todo todo = new Todo();
-        todo.setTitle("Todo to Insert");
-        todo.setStatus(Status.valueOf("未着手"));
-        todo.setDetails("hogehoge");
-
+        // テスト用のTodoオブジェクトをセットアップ
+        Todo todo = setUpTodo();
+        // Todoをマッパーを通じて挿入
         todoMapper.insertTodo(todo);
+
+        // TodoのIDがnullでないことを確認
         assertThat(todo.getId(), is(notNullValue()));
 
+        System.out.println("Inserted Todo ID: " + todo.getId());
+
+        // 挿入後のTodoをIDを使用して再度取得
         Todo insertedTodo = todoMapper.getTodoById(todo.getId());
+
+        System.out.println("Fetched Todo ID: " + insertedTodo.getId());
+
+        // 取得したTodoがnullでないことを確認
         assertThat(insertedTodo, is(notNullValue()));
-        assertThat(insertedTodo.getTitle(), is(equalTo("Todo to Insert")));
+        // 取得したTodoのタイトルが"Test Todo"であることを確認
+        assertThat(insertedTodo.getTitle(), is(equalTo("Test Todo")));
     }
 
+    // Todoを更新するテスト
     @Test
     void updateTodo() {
-        Todo todo = new Todo();
+        // テスト用のTodoオブジェクトをセットアップ
+        Todo todo = setUpTodo();
+        // Todoをマッパーを通じて挿入
+        todoMapper.insertTodo(todo);
 
+        // 更新前のTodoをIDを使用して再度取得
+        Todo beforeUpdate = todoMapper.getTodoById(todo.getId());
+        // 更新前のTodoのタイトルが"Test Todo"であることを確認
+        assertThat(beforeUpdate.getTitle(), is(equalTo("Test Todo")));
+
+        // Todoのタイトルを更新
+        todo.setTitle("Updated Todo");
+        // Todoをマッパーを通じて更新
+        todoMapper.updateTodo(todo);
+
+        // 更新後のTodoをIDを使用して再度取得
+        Todo afterUpdate = todoMapper.getTodoById(todo.getId());
+        // 更新後のTodoのタイトルが"Updated Todo"であることを確認
+        assertThat(afterUpdate.getTitle(), is(equalTo("Updated Todo")));
     }
 
+    // IDに基づいてTodoを削除するテスト
     @Test
     void deleteTodoById() {
+        // テスト用のTodoオブジェクトをセットアップ
+        Todo todo = setUpTodo();
+        // Todoをマッパーを通じて挿入
+        todoMapper.insertTodo(todo);
+
+        // 削除前のTodoをIDを使用して再度取得
+        Todo beforeDelete = todoMapper.getTodoById(todo.getId());
+        // 削除前のTodoがnullでないことを確認
+        assertThat(beforeDelete, is(notNullValue()));
+
+        // TodoをIDを使用して削除
+        todoMapper.deleteTodoById(todo.getId());
+
+        // 削除後のTodoをIDを使用して再度取得
+        Todo afterDelete = todoMapper.getTodoById(todo.getId());
+        // 削除後のTodoがnullであることを確認
+        assertThat(afterDelete, is(nullValue()));
     }
 
+    @Disabled
     @Test
     void getTodoSortedById() {
     }
 
+    @Disabled
     @Test
     void getTodoSortedByStatus() {
     }
---
+++ src/test/java/com/example/todoapi2/service/TodoServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.example.todoapi2.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoServiceImplTest {
+
+    @Test
+    void getAllTodos() {
+    }
+
+    @Test
+    void getTodoByStatus() {
+    }
+
+    @Test
+    void getTodoByTitle() {
+    }
+
+    @Test
+    void getTodoById() {
+    }
+
+    @Test
+    void createTodo() {
+    }
+
+    @Test
+    void updateTodo() {
+    }
+
+    @Test
+    void deleteTodoById() {
+    }
+
+    @Test
+    void getTodoSortedById() {
+    }
+
+    @Test
+    void getTodoSortedByStatus() {
+    }
+}
